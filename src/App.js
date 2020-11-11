7277990f3a9acc67ed6dcee2fcc424fcf67f3c8e
Vo Tan <vdt040499@gmail.com>
2020-11-11 08:57:03 +0700

    Switch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import { Link, NavLink, Route } from 'react-router-dom';
-import './App.css';
+import { Link, NavLink, Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import TodoFeature from './features/Todo';
-import AlbumFeature from './features/Album';
+import TodoFeature from "./features/Todo";
+import AlbumFeature from "./features/Album";
 
 function App() {
   return (
     <div className="App">
-        <p><Link to="/todos">Todos</Link></p>
-        <p><Link to="/albums">Albums</Link></p>
+      <p>
+        <NavLink to="/todos" activeClassName="active-menu">
+          Todos
+        </NavLink>
+      </p>
+      <p>
+        <NavLink to="/albums">Albums</NavLink>
+      </p>
 
-        <p><NavLink to="/todos" activeClassName="active-menu">Todos</NavLink></p>
-        <p><NavLink to="/albums">Albums</NavLink></p>
-
+      <Switch>
+        <Route path="/" component={TodoFeature} />
         <Route path="/todos" component={TodoFeature} />
-        <Route path="/albums"component={AlbumFeature}/>
+        <Route path="/albums" component={AlbumFeature} />
+      </Switch>
+
+      {/* <Route path="/" component={TodoFeature} />
+      <Route path="/todos" component={TodoFeature} />
+      <Route path="/albums" component={AlbumFeature} /> */}
     </div>
   );
 }
