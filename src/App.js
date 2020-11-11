485958291246c041ee7941fc3f8abd1b4d951514
Vo Tan <vdt040499@gmail.com>
2020-11-11 08:46:44 +0700

    Link NavLink

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { Route } from 'react-router-dom';
+import { Link, NavLink, Route } from 'react-router-dom';
+import './App.css';
+
 import TodoFeature from './features/Todo';
 import AlbumFeature from './features/Album';
 
 function App() {
   return (
     <div className="App">
+        <p><Link to="/todos">Todos</Link></p>
+        <p><Link to="/albums">Albums</Link></p>
+
+        <p><NavLink to="/todos" activeClassName="active-menu">Todos</NavLink></p>
+        <p><NavLink to="/albums">Albums</NavLink></p>
+
         <Route path="/todos" component={TodoFeature} />
         <Route path="/albums"component={AlbumFeature}/>
     </div>
