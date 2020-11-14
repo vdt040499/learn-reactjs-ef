629765eeae70c9c9ba0a8474e259e9609a685b8f
Vo Tan <vdt040499@gmail.com>
2020-11-14 19:51:23 +0700

    Material Styles

--- src/features/Counter/index.jsx
+++ src/features/Counter/index.jsx
@@ -2,10 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { decrease, increase } from './counterSlice';
+import { Button, makeStyles } from '@material-ui/core';
 
 CounterFeature.propTypes = {};
 
+const useStyles = makeStyles({
+    root: {
+        background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)',
+        color: 'white',
+        height: 32,
+        padding: '0 30px',
+    }
+})
+
 function CounterFeature() {
+    const classes = useStyles()
     const counter = useSelector(state => state.counter)
     const dispatch = useDispatch();
 
@@ -24,8 +38,8 @@ function CounterFeature() {
             Counter: {counter}
 
             <div>
-                <button onClick={handleIncreaseClick}>Increase</button>
-                <button onClick={handleDecreaseClick}>Decrease</button>
+                <Button className={classes.root} onClick={handleIncreaseClick}>Increase</Button>
+                <Button className={classes.root} onClick={handleDecreaseClick}>Decrease</Button>
             </div>
         </div>
     );
