1b464b884b0e7ede96ac2fe88a1c5a6c6f76f712
Vo Tan <vdt040499@gmail.com>
2020-11-17 19:40:37 +0700

    Setup Registration API

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
+import userReducer from '../features/Auth/userSlice';
 import counterReducer from '../features/Counter/counterSlice';
 const { configureStore } = require('@reduxjs/toolkit');
 
 const rootReducer = {
   counter: counterReducer,
+  user: userReducer
 };
 
 const store = configureStore({
